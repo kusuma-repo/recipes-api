a28832619748221846fb75426a8055703fe3cea5
faldi kusuma <ifaltiga.pm@gmail.com>
2020-09-13 20:13:44 +0700

    Updates

---
+++ precache-manifest.ef623893f64eeb306c23640989785ad7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "102b580be9b077c3d00d094b29b92f3e",
+    "url": "/recipes-api/index.html"
+  },
+  {
+    "revision": "83871bf1c6079d9b4207",
+    "url": "/recipes-api/static/css/main.6a65e2bb.chunk.css"
+  },
+  {
+    "revision": "8f973d4319794b0e694a",
+    "url": "/recipes-api/static/js/2.21e8189d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipes-api/static/js/2.21e8189d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83871bf1c6079d9b4207",
+    "url": "/recipes-api/static/js/main.a7561ac6.chunk.js"
+  },
+  {
+    "revision": "93f1cbb3a90c2f0bb599",
+    "url": "/recipes-api/static/js/runtime-main.d1f0bf5c.js"
+  }
+]);
